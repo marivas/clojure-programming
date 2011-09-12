ededf8f601eda1cc847b5c595c1d62d1971154b5
Manuel Rivas <marivas11@gmail.com>
2011-09-12 21:48:48 +0200

    more ...

--- src/clojure_programming/ch2.clj
+++ src/clojure_programming/ch2.clj
@@ -11,3 +11,22 @@
 ((multiplier 0.3) 100) ; => 30.0
 
 
+(doc rand-int)
+
+(map inc [1 2 3 4])
+
+(map vector [1 2 3 4] [ "a" "b" "c" "d" ])
+
+(reduce + 50 [1 2 3 4])
+
+(reduce
+ (fn [m v]
+   (assoc m v (str v)))
+ {}
+ [1 2 3 4])
+
+
+(-> v z/vector-zip z/down z/node)
+(-> v z/vector-zip)
+
+
