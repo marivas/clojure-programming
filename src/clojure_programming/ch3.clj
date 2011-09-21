0e24d9799656b0bb25470151ce9d9f258429ecdf
Manuel Rivas <marivas11@gmail.com>
2011-09-21 22:29:54 +0200

    map funcs

--- src/clojure_programming/ch3.clj
+++ src/clojure_programming/ch3.clj
@@ -21,3 +21,56 @@
   (str "53/7=" q "*7+" r))
 
 
+(def data {:title "Elephant", :artist "The White Stripes", :year 2003})
+
+
+(def playlist [{:title "Elephant", :artist "The White Stripes",
+                :year 2003}
+               {:title "Helioself", :artist "Papas Fritas",
+                :year 1997}
+               {:title "Stories from the City, Stories from the Sea",
+                :artist "PJ Harvey", :year 2000}
+               {:title "Zen Rodeo", :artist "Mardi Gras BB",
+                :year 2002}])
+
+(map :title playlist)
+
+(defn summarize1 [album]
+  (str (:title album) " / " (:artist album) " / " (:year album)))
+
+(defn summarize2 [{t :title a :artist y :year}] (str t "/" a "/" y))
+
+(defn summarize3 [{:keys [title artist year]}]
+  (str title " / " artist " / " year))
+
+(map summarize3 playlist)
+
+;;; other maps usages
+(group-by #(rem % 3) (range 10))
+
+(def albums [{:title "Elephant", :artist "Papas Fritas",
+                :year 2003}
+               {:title "Helioself", :artist "Papas Fritas",
+                :year 1997}
+               {:title "Stories from the City, Stories from the Sea",
+                :artist "PJ Harvey", :year 2000}
+               {:title "Zen Rodeo", :artist "Mardi Gras BB",
+                :year 2002}])
+
+
+(group-by :artist albums)
+(group-by (juxt :artist :year) albums) ;; TODO hacer mas pruebas
+
+(keys  (into {} (for [[k v] (group-by :artist albums)]
+                  [k (summarize3 (get v 0))])))
+
+
+
+(defn reduce-by [key-fn f init coll]
+  (reduce (fn [summaries x]
+            (let [k (key-fn x)]
+              (assoc summaries k (conj (summaries k init) (f x)))))
+          {} coll ))
+
+(reduce-by :artist summarize3 [] albums)
+
