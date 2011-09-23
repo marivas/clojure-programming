54afc5db6b835ba01fbebda7dd9aed077fe348aa
Manuel Rivas <marivas11@gmail.com>
2011-09-23 22:22:56 +0200

    more maps

--- src/clojure_programming/ch3.clj
+++ src/clojure_programming/ch3.clj
@@ -66,11 +66,72 @@
 
 
 
-(defn reduce-by [key-fn f init coll]
+(defn reduce-by2 [key-fn f init coll]
   (reduce (fn [summaries x]
             (let [k (key-fn x)]
               (assoc summaries k (conj (summaries k init) (f x)))))
           {} coll ))
 
-(reduce-by :artist summarize3 [] albums)
+(reduce-by2 :artist summarize3 [] albums)
 
+(defn reduce-by [key-fn f init coll]
+  (reduce (fn [summaries x]
+            (let [k (key-fn x)]
+              (assoc summaries k (f (summaries k init) x))))
+          {} coll))
+
+(def orders
+  [{:product "Clock", :customer "Wile Coyote", :qty 6, :total 300}
+   {:product "Dynamite", :customer "Wile Coyote", :qty 20, :total 5000}
+   {:product "Shotgun", :customer "Elmer Fudd", :qty 2, :total 800}
+   {:product "Cartridges pack", :customer "Elmer Fudd", :qty 4, :total 100}
+   {:product "Hole", :customer "Wile Coyote", :qty 1, :total 1000}
+   {:product "Anvil", :customer "Elmer Fudd", :qty 2, :total 300}
+   {:product "Anvil", :customer "Wile Coyote", :qty 6, :total 900}])
+
+(reduce-by :customer #(+ %1 (:total %2)) 0 orders)
+(reduce-by :product #(conj %1 (:customer %2)) #{} orders)
+
+(reduce-by (juxt :customer :product)
+           #(+ %1 (:total %2)) 0 orders)
+
+(defn reduce-by-in [keys-fn f init coll]
+  (reduce (fn [summaries x]
+            (let [ks (keys-fn x)]
+              (assoc-in summaries ks
+                        (f (get-in summaries ks init) x))))
+          {} coll))
+
+(reduce-by-in (juxt :customer :product)
+              #(+ %1 (:total %2)) 0 orders)
+
+(def flat-breakup {["Wile Coyote" "Anvil"] 900,
+                   ["Elmer Fudd" "Anvil"] 300,
+                   ["Wile Coyote" "Hole"] 1000,
+                   ["Elmer Fudd" "Cartridges pack"] 100,
+                   ["Elmer Fudd" "Shotgun"] 800,
+                   ["Wile Coyote" "Dynamite"] 5000,
+                   ["Wile Coyote" "Clock"] 300})
+
+
+(reduce #(apply assoc-in %1 %2) {} flat-breakup)
+
+(find {:ethe1 nil} :lucy) ; => nil
+(val (find {:ethe1 nil} :ethe1)) ;=> [:ethe1 nil] => nil
+
+(if-let [e (find {:ethe1 "a"} :ethe1)]
+  (str "found: " (val e))
+  "not found")
+
+(if-let [[_ v] (find {:ethe1 "a"} :ethe1)]
+  (str "found: " v)
+  "not found")
+
+(contains? {:ethe1 nil} :ethe1)
+
+(def order { :customer {:name "Informa" "addresses" "Alcobendas"}})
+
+(if-let [[customer address]
+         (find  (:customer order) "addresses" )]
+  (str customer \newline address)
+  "no address for this order")
